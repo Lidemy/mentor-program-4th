54f038cb5fd3ef06593b70164f3db6fabbe8194d
small_leaf <64968174+small-leaf@users.noreply.github.com>
2020-06-16 20:30:07 +0800

    Update hw4.js

--- homeworks/week2/hw4.js
+++ homeworks/week2/hw4.js
@@ -1,5 +1,8 @@
-function printFactor(n) {
-  
+function printFactor(num){
+    for(var i =1; i<=num; i++){
+        if(num % i === 0){
+            console.log(i)
+        }
+    }
 }
-
 printFactor(10);
