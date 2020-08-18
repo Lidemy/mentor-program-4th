c1c8f35eea8301ec8c061f1e28b973e9018ea090
krebikshaw <krebikshaw@gmail.com>
2020-08-18 14:50:02 +0800

    week2-4_hw

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,9 @@
 function printStars(n) {
-  
+	let str=''
+	for(let i=0;i<n;i++){
+		str='*'
+		console.log(str) 
+	}
 }
 
 printStars(5);
--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,10 @@
 function reverse(str) {
-  
+	let ans=''
+	let length=str.length
+	for(let i=length-1;i>=0;i--){
+		ans+=str[i]
+	}
+	console.log(ans)
 }
 
 reverse('hello');
